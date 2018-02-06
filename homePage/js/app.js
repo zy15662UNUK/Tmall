77dc87018fe8fd4eebcf57f5d203d4e8b6a50502
Jiahao LUO <2679229323@qq.com>
2018-02-06 23:37:55 +0000

    sideMenu position control by JS

--- homePage/js/app.js
+++ homePage/js/app.js
@@ -2,6 +2,24 @@
 $('.carousel').carousel({
   interval: 3000
 });
+//side menu
 $(".eachCategory").hover(function() {
   $(".productsAsideCategorys").toggleClass('show');
+});
+function sideMenuTop(){
+  var categoryMenu_top = parseInt($(".homePage-nav").offset().top)+$(".homePage-nav").height();
+  console.log(categoryMenu_top);
+  $(".categoryMenu").css("top",categoryMenu_top+"px");
+  $(".productsAsideCategorys").css("top", categoryMenu_top+"px");
+}
+sideMenuTop();
+//cat ear
+$(".nav-item").hover(function() {
+  var left = parseInt($(this).offset().left)+20;
+  var top = parseInt($(this).offset().top)-10;
+  $("#catear").css('left', left+"px");
+  $("#catear").css('top', top+"px");
+  $("#catear").css("display","block");
+},function(){
+  $("#catear").hide();
 });
