7af6aee93fc754b505b7a310bfb62d628f22d852
Jiahao LUO <2679229323@qq.com>
2018-02-07 20:56:37 +0000

    rank and price

--- homePage/js/app.js
+++ homePage/js/app.js
@@ -3,23 +3,27 @@ $('.carousel').carousel({
   interval: 3000
 });
 //side menu
-$(".eachCategory").hover(function() {
-  $(".productsAsideCategorys").toggleClass('show');
-});
-function sideMenuTop(){
+
+function sideMenu(){
+  $(".eachCategory").hover(function() {
+    $(".productsAsideCategorys").toggleClass('show');
+  });
   var categoryMenu_top = parseInt($(".homePage-nav").offset().top)+$(".homePage-nav").height();
   console.log(categoryMenu_top);
   $(".categoryMenu").css("top",categoryMenu_top+"px");
   $(".productsAsideCategorys").css("top", categoryMenu_top+"px");
 }
-sideMenuTop();
+sideMenu();
 //cat ear
-$(".nav-item").hover(function() {
-  var left = parseInt($(this).offset().left)+20;
-  var top = parseInt($(this).offset().top)-10;
-  $("#catear").css('left', left+"px");
-  $("#catear").css('top', top+"px");
-  $("#catear").css("display","block");
-},function(){
-  $("#catear").hide();
-});
+function catear(){
+  $(".nav-item").hover(function() {
+    var left = parseInt($(this).offset().left)+20;
+    var top = parseInt($(this).offset().top)-10;
+    $("#catear").css('left', left+"px");
+    $("#catear").css('top', top+"px");
+    $("#catear").css("display","block");
+  },function(){
+    $("#catear").hide();
+  });
+}
+catear();
