5c31c0b6300e23e79bedb4d1711c65833b443c1f
Jiahao LUO <2679229323@qq.com>
2018-02-11 13:11:02 +0000

    finsih cart page

--- cartPage/js/app.js
+++ cartPage/js/app.js
@@ -1,83 +1,103 @@
 
 function checkbox(){
   //Click select all
-    var selectAll = false;
     $(".selectAllItem").click(function(event) {
-      if(!selectAll){
+      if($(".selectAllItem").attr('src')=='http://how2j.cn/tmall/img/site/cartNotSelected.png'){
         $(".selectAllItem").attr('src', 'http://how2j.cn/tmall/img/site/cartSelected.png');
+        //check each check box also
         $(".cartProductItemIfSelected").attr('src', 'http://how2j.cn/tmall/img/site/cartSelected.png');
-        var totalNum = 
-        $(".cartSumNumber").text(totalNum+"");
-        selectAll = true;
       }else{
-        selectAll = false;
         $(".selectAllItem").attr('src', 'http://how2j.cn/tmall/img/site/cartNotSelected.png');
         $(".cartProductItemIfSelected").attr('src', 'http://how2j.cn/tmall/img/site/cartNotSelected.png');
       }
       handleButton();
     });
-
   // click each check box
-    var selectNum = $(".cartProductItemIfSelected").length;
+    var selectNum = $(".cartProductItemIfSelected").length;//Record num of seleted items
   $(".cartProductItemIfSelected").each(function() {
-    var select = false;
     $(this).click(function(event) {
-      if(!select){
+      if($(this).attr('src')==='http://how2j.cn/tmall/img/site/cartNotSelected.png'){
         $(this).attr('src', 'http://how2j.cn/tmall/img/site/cartSelected.png');
-        select = true;
         selectNum--;
       }else{
-        select = false;
         $(this).attr('src', 'http://how2j.cn/tmall/img/site/cartNotSelected.png');
         selectNum++;
+      }
+      if(selectNum === 0){
+        $(".selectAllItem").attr('src', 'http://how2j.cn/tmall/img/site/cartSelected.png');
+      }else{
+        $(".selectAllItem").attr('src', 'http://how2j.cn/tmall/img/site/cartNotSelected.png');
       }
       handleButton();
     });
   });
   function handleButton(){
-    if((selectAll == true) || (selectNum < $(".cartProductItemIfSelected").length)){
+    if(($(".selectAllItem").attr('src')=='http://how2j.cn/tmall/img/site/cartSelected.png') || (selectNum < $(".cartProductItemIfSelected").length)){
       $(".createOrderButton").addClass('buttonPaymentReady');
-      if(selectNum === 0){
-        $(".selectAllItem").attr('src', 'http://how2j.cn/tmall/img/site/cartSelected.png');
-      }
     }else{
       $(".createOrderButton").removeClass('buttonPaymentReady');
     }
   }
 }
 checkbox();
-
 //Change input box value
 function handleInput(){
   //change overall num of products and overall price, when the selection has been made
-  function handlePrice(){
-    var totalNum = 0,totalPrice = 0;
-    $(".cartProductItemTR").each(function() {
-      var checkbox = $(this).find('.cartProductItemIfSelected');
-      if(checkbox.attr('src')==="http://how2j.cn/tmall/img/site/cartSelected.png"){
-        var plus = parseInt($(this).find(".orderItemNumberSetting").val());
-        totalNum += plus;
-      }
-
-      $(".cartSumNumber").text(totalNum+"");
-      $(".cartSumPriceNum").val(totalPrice+"");
-    });
-  }
-
   $(".cartProductChangeNumberDiv").each(function() {
     var num = 1;
     $(".numberMinus").click(function(event) {
       if (num>1){
         num --;
-        handlePrice();
       }
       $(this).siblings('.orderItemNumberSetting').val(num+"");
     });
     $(".numberPlus").click(function(event) {
       num++;
       $(this).siblings('.orderItemNumberSetting').val(num+"");
-      handlePrice();
     });
   });
 }
 handleInput();
+//change overall num of products and overall price, when the selection has been made
+function priceAndNum(){
+  $(".numberMinus").click(function(event){
+    handlePrice();
+  });
+
+  $(".numberPlus").click(function(event){
+    handlePrice();
+  });
+  $(".selectAllItem").click(function(event) {
+    handlePrice();
+  });
+  $(".cartProductItemIfSelected").click(function(event) {
+    handlePrice();
+  });
+  function handlePrice(){
+    var totalNum = 0,totalPrice = 0;
+    $(".cartProductItemTR").each(function() {
+      var checkbox = $(this).find('.cartProductItemIfSelected');
+      if(checkbox.attr('src')==="http://how2j.cn/tmall/img/site/cartSelected.png"){
+        var numPlus = parseInt($(this).find(".orderItemNumberSetting").val());
+        var pricePlusStr = $(this).find(".cartProductItemSmallSumPrice").text();
+        //remove "," and "￥"
+        pricePlusStr = pricePlusStr.replace(/,/g, '');
+        pricePlusStr = pricePlusStr.replace(/￥/g, '');
+        console.log(pricePlusStr);
+        var pricePlus = parseFloat(pricePlusStr);
+        totalNum += numPlus;
+        totalPrice += pricePlus*numPlus;
+      }
+      $(".cartSumNumber").text(totalNum+"");
+      $(".cartSumPriceNum").text(totalPrice+"");
+    });
+  }
+}
+priceAndNum();
+//delete item
+function deleteItem(){
+  $(".deleteOrderItem").click(function(event) {
+    $(this).parents(".cartProductItemTR").remove();
+  });
+}
+deleteItem();
