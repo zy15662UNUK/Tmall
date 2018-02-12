018c0d8f46e1bfe9446406126c9791f78db2ae3c
Jiahao LUO <2679229323@qq.com>
2018-02-12 19:26:12 +0000

    order page finish

--- orderPage/js/app.js
+++ orderPage/js/app.js
@@ -1,4 +1,4 @@
-// top bar manipulation
+// top bar css style manipulation
 $(".orderType div").click(function(event) {
   if(!$(this).hasClass('selectedOrderType')){
     $(".orderType div").each(function() {
@@ -7,5 +7,20 @@ $(".orderType div").click(function(event) {
       }
     });
     $(this).addClass('selectedOrderType');
+  }
+});
+// product list show/display according to top bar selection
+$(".orderType div").click(function(event) {
+  if($(this).find('a').attr('orderstatus')==="all"){
+    $(".orderListItemTable").show();
+  }else{
+    var filter = $(this).find('a').attr('orderstatus');
+    $(".orderListItemTable").each(function() {
+      if($(this).attr('orderstatus')===filter){
+        $(this).show();
+      }else{
+        $(this).hide();
+     }
+    });
   }
 });
